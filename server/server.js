3c167fceea1bd2ea8774eb0cd6d2c64272b22a99
Erit Salgado <erit@torodigital.com.mx>
2021-09-17 13:30:09 -0500

    Callback añadido

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on("connect", (client) => {
     client.on('mensaje', (data, callback) => {
         client.broadcast.emit('mensaje', data, port);
         console.log(data);
-        // callback(port);
+        callback(data);
     })
 
     client.on("disconnect", () =>{
